8c1413b57c48310f99d3d6551ba5f399ba8be67b
Lucca <luccazvps@gmail.com>
2023-01-25 16:02:40 -0300

    feat: connect to mongodb

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { ApolloServer, gql } from "apollo-server";
+import mongoose from "mongoose";
+import { resolvers, typeDefs } from "./rootTypesDefs";
+import dotenv from "dotenv";
+import { userSchema } from "./db/user-schema";
+dotenv.config();
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log("Connected!"));
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: {
+    userModel: mongoose.model("graphqlusers", userSchema),
+  },
+});
+server.listen(8000).then(() => {
+  console.log("Apollo iniciado");
+});
