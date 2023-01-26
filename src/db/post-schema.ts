f97031bd6d40120dfb5671c14fe2c6854260a546
Lucca <luccazvps@gmail.com>
2023-01-26 15:35:07 -0300

    feat: add post mongoose schema

---
+++ src/db/post-schema.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+export const postSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    body: { type: String, required: true },
+    categories: [{ type: String, required: true }],
+    user: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "graphusers",
+      },
+    ],
+  },
+  { collection: "graphposts", timestamps: true }
+);
+export const postModel = mongoose.model("graphqlusers", postSchema);
