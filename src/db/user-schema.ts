87e3232f45b6b4fbc779a6130bd154759f810a8e
Lucca <luccazvps@gmail.com>
2023-01-26 10:05:18 -0300

    feat: create user schema

--- src/db/user-schema.ts
+++ src/db/user-schema.ts
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 export const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true },
+    name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
   },
   { collection: "graphusers" }
 );
+export const userModel = mongoose.model("graphqlusers", userSchema);
