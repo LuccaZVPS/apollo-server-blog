0cab826d965921858536667057375b709a4d9ed6
Lucca <luccazvps@gmail.com>
2023-01-26 15:37:04 -0300

    feat: add new auth context

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,16 @@ import { ApolloServer, gql } from "apollo-server";
 import mongoose from "mongoose";
 import { resolvers, typeDefs } from "./rootTypesDefs";
 import dotenv from "dotenv";
-import { userSchema } from "./db/user-schema";
+import { context } from "./middlewares/auth";
 dotenv.config();
 mongoose
   .connect(process.env.DATABASE_URL)
   .then(() => console.log("Connected!"));
-
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context,
 });
-server.listen(8000).then(() => {
+server.listen(8080).then(() => {
   console.log("Apollo iniciado");
 });
