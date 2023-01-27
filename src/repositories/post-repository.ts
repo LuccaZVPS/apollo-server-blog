7054a34e624cab370f89a763ee79caf5fdd4dbde
Lucca <luccazvps@gmail.com>
2023-01-27 09:46:18 -0300

    feat: add findByAdd method

--- src/repositories/post-repository.ts
+++ src/repositories/post-repository.ts
@@ -1,6 +1,7 @@
 import { CreatePostDTO } from "../use-cases/posts/DTOs/create-post";
 import { postModel } from "../db/post-schema";
-import mongoose from "mongoose";
+import mongoose, { Unpacked } from "mongoose";
+import { Post } from "../domain/post";
 export class PostRepository {
   async createPost(data: CreatePostDTO, userId) {
     return await postModel.create({
@@ -12,11 +13,20 @@ export class PostRepository {
   }
 
   async update(data: CreatePostDTO, postId) {
-    const post = await postModel.findOneAndUpdate(
+    return await postModel.findOneAndUpdate(
       { _id: new mongoose.Types.ObjectId(postId) },
       { ...data },
       { new: true }
     );
-    return post;
+  }
+  async findById(id: string): Promise<Post | undefined> {
+    const post = await postModel.findOne({
+      _id: new mongoose.Types.ObjectId(id),
+    });
+
+    console.log(post);
+    if (post) {
+      return post as unknown as Post;
+    }
   }
 }
